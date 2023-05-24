66ef7580b4451a4a7df41b7c61f301ad07142cbc
konotopdv <konotopdv@gmail.com>
2023-05-24 10:47:08 +0200

    Update Task02

--- js/task-02.js
+++ js/task-02.js
@@ -17,29 +17,12 @@ const ingredients = [
   "Condiments",
 ];
 const ingredientsEL = document.querySelector("#ingredients");
-const li1El = document.createElement("li");
-li1El.classList.add("item");
-li1El.textContent = "Potatoes";
-console.log(li1El);
-const li2El = document.createElement("li");
-li2El.classList.add("item");
-li2El.textContent = "Mushrooms";
-console.log(li2El);
-const li3El = document.createElement("li");
-li3El.classList.add("item");
-li3El.textContent = "Garlic";
-console.log(li3El);
-const li4El = document.createElement("li");
-li4El.classList.add("item");
-li4El.textContent = "Tomatos";
-console.log(li4El);
-const li5El = document.createElement("li");
-li5El.classList.add("item");
-li5El.textContent = "Herbs";
-console.log(li5El);
-const li6El = document.createElement("li");
-li6El.classList.add("item");
-li6El.textContent = "Condiments";
-console.log(li6El);
-ingredientsEL.append(li1El, li2El, li3El, li4El, li5El, li6El);
-console.log(ingredientsEL);
+
+const itemsEl = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+ingredientsEL.append(...itemsEl);
